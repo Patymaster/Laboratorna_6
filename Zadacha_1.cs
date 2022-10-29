b6c8edd6bd90424e89131976dc8f0a86946b0a9f
Patymaster <62792625+Patymaster@users.noreply.github.com>
2022-10-29 14:19:17 +0300

    Add files via upload

---
+++ Zadacha_1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class MyList<T>
+    {
+        private T[] myList = null;
+
+        public T this[int index]
+        {
+            get { return myList[index]; }
+            set { myList[index] = value; }
+        }
+
+        public MyList()
+        {
+            this.myList = new T[1];
+        }
+
+        public MyList(int count)
+        {
+            this.myList = new T[count];
+        }
+
+        public void Add(T item)
+        {
+            T[] extendedList = new T[myList.Length + 1];
+            extendedList[extendedList.Length - 1] = item;
+            myList = extendedList;
+        }
+
+        public int Capacity
+        {
+            get { return myList.Length; }
+
+        }
+
+        public int Count
+        {
+            get
+            {
+                int count = 0;
+                for (int i = 0; i < myList.Length; i++)
+                {
+                    if (myList[i].ToString() != null)
+                    {
+                        count++;
+                    }
+                }
+                return count;
+            }
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> mylist = new MyList<int>(5);
+            mylist[0] = 1;
+            Console.WriteLine("Ємнiсть списку: {0} елемент(iв)", mylist.Capacity);
+            Console.WriteLine("Список фактично мiстить: {0} елемент(iв)", mylist.Count);
+            mylist.Add(15);
+            Console.WriteLine("Ємнiсть списку: {0} елемент(iв)", mylist.Capacity);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Zadacha_4.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    public class MyFactory<T> where T : new()
+    {
+        public static T FacrotyMethod()
+        {
+            return new T();
+        }
+    }
+
+    public class MyType
+    {
+        public int a;
+
+        public MyType(int a)
+        {
+            this.a = a;
+        }
+
+    }
+    public class MyClass
+    {
+        public int Val { get; set; }
+        public MyClass(int i)
+        {
+            Val = i;
+        }
+        public MyClass() { }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var instance = MyFactory<MyType>.FacrotyMethod();
+
+            Console.WriteLine(instance.GetType());
+            Console.ReadKey();
+        }
+    }
+}
